476cec04f1b45cc102dc7f9f836e5e3a03b2ebe5
Hyapp <282144363@qq.com>
2020-04-15 01:03:17 +0800

    Copy the frame buffer code

--- DirectX12Test01/DirectX12Test01/source/main.cpp
+++ DirectX12Test01/DirectX12Test01/source/main.cpp
@@ -17,11 +17,20 @@
 //	return Win32Application::Run(&sample, hInstance, nCmdShow);
 //}
 
-#include <ConstBuffer.h>
+//#include <ConstBuffer.h>
+//
+//_Use_decl_annotations_
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+//{
+//	D3D12HelloConstBuffer sample(1280, 720, L"D3D12 Hello Constant Buffer");
+//	return Win32Application::Run(&sample, hInstance, nCmdShow);
+//}
+
+#include <TestFrameBuffer.h>
 
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-	D3D12HelloConstBuffer sample(1280, 720, L"D3D12 Hello Constant Buffer");
+	D3D12HelloFrameBuffering sample(1280, 720, L"D3D12 Hello Frame Buffering");
 	return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
